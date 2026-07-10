2f86f59c6d82ef5ce336fdfa9b5465d004b505c0
Ohminseok <oms50112@gmail.com>
2026-07-11 01:07:14 +0900

    두수의 차구하기

---
+++ src/main/java/kopo/Level/Level0/Multiply.java
@@ -0,0 +1,17 @@
+package kopo.Level.Level0;
+
+public class Multiply {
+    public static void main(String[] args) {
+        /*
+        정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+         */
+        int num1 = 3;
+        int num2 = 3;
+
+        System.out.println(solution(num1,num2));
+    }
+    static int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
+
