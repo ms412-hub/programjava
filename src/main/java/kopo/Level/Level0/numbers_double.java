aa594547879ea6ab6d7c0bfff67c41e14d514253
Ohminseok <oms50112@gmail.com>
2026-07-16 20:23:21 +0900

    사이트에선 바로 실행되는데 여기선 실행하는법 찾기 힘들 아서 감섹히다.

---
+++ src/main/java/kopo/Level/Level0/numbers_double.java
@@ -0,0 +1,24 @@
+package kopo.Level.Level0;
+
+import java.util.Arrays;
+
+public class numbers_double {
+    public static void main(String[] args) {
+        /*
+        정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+         */
+        int[] num = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(solution(num)));
+    }
+
+    static int[] solution(int[] num) {
+        int[] a = new int[num.length];
+
+        for (int i = 0; i < num.length; i++) {
+            a[i] = num[i] * 2;
+        }
+
+        return a;
+    }
+}
