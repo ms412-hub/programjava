8972f4b5236161e7fdd92634d7115ecf3bcdc0df
Ohminseok <oms50112@gmail.com>
2026-07-16 20:26:34 +0900

    출력하는법 이런식으로 하지말고 조금더 간소화가 가능할가

--- src/main/java/kopo/Level/Level0/numbers_double.java
+++ src/main/java/kopo/Level/Level0/numbers_double.java
@@ -7,18 +7,16 @@ public class numbers_double {
         /*
         정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
          */
-        int[] num = {1, 2, 3, 4, 5};
+        int[] num = {4};
 
         System.out.println(Arrays.toString(solution(num)));
     }
 
     static int[] solution(int[] num) {
         int[] a = new int[num.length];
-
         for (int i = 0; i < num.length; i++) {
             a[i] = num[i] * 2;
         }
-
         return a;
     }
 }
