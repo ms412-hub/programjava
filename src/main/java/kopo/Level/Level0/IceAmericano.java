686164fccd07f50761fed9d871aa17dbe2f83bc2
Ohminseok <oms50112@gmail.com>
2026-07-19 16:35:40 +0900

    아이스 아메리카노 구하기

---
+++ src/main/java/kopo/Level/Level0/IceAmericano.java
@@ -0,0 +1,21 @@
+package kopo.Level.Level0;
+
+import java.util.Arrays;
+
+public class IceAmericano {
+    public static void main(String[] args) {
+        /*
+        머쓱이는 추운 날에도 아이스 아메리카노만 마십니다. 아이스 아메리카노는 한잔에 5,500원입니다. 머쓱이가 가지고 있는 돈 money가 매개변수로 주어질 때, 머쓱이가 최대로 마실 수 있는 아메리카노의 잔 수와 남는 돈을 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+         */
+        int pay = 15000;
+
+        System.out.println(Arrays.toString(solution(pay)));
+    }
+    static int[] solution(int pay) {
+        int[] don = {pay / 5500, pay % 5500};
+        return don;
+    }
+}
+
+
+
