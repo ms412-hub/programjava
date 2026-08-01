a5b54545b11d1e45b5160f64ec32b78b22b44211
Ohminseok <oms50112@gmail.com>
2026-08-01 22:33:12 +0900

    피자

--- src/main/java/kopo/Level/Level0/PizzaSlice1.java
+++ src/main/java/kopo/Level/Level0/PizzaSlice1.java
@@ -1,9 +1,9 @@
 package kopo.Level.Level0;
 
-public class SevenSlicePizza {
+public class PizzaSlice1 {
     public static void main(String[] args) {
         /*
-        머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 2022년 기준 선생님의 나이 age가 주어질 때, 선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
+        머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 주어질 때, 모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 return 하는 solution 함수를 완성해보세요.
          */
 
         int n=15;
