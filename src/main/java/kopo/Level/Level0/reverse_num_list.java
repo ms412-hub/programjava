dcd516eca502fd5ce52e72f8045be3972f9527d1
Ohminseok <oms50112@gmail.com>
2026-08-10 22:24:45 +0900

    이렇게 하는게 맞나

--- src/main/java/kopo/Level/Level0/reverse_num_list.java
+++ src/main/java/kopo/Level/Level0/reverse_num_list.java
@@ -5,6 +5,8 @@ public class reverse_num_list {
         /*
         정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
          */
+        int[] num_list = {1, 2, 3, 4, 5};
+
 }
     static int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length];
